f0008f749070f05730c789ea41958261d0d381cd
chi liu <cliu_whu@yeah.net>
2017-12-03 15:19:58 +0800

    commit the dataStructure of some problem

---
+++ LeetCode/dataStructure/ListNode.java
@@ -0,0 +1,13 @@
+/**
+ * ListNode
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        this.val = x;
+    }
+    
+}
